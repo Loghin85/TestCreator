0e9f69aedca2551e14770536da918fc920aad9ff
Andrei Loghin <soricel98@yahoo.com>
2021-03-03 02:20:08 +0200

    gmail setup update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,11 +13,12 @@ Rails.application.configure do
 
 	# SMTP settings for gmail
 	config.action_mailer.smtp_settings = {
-		:address              => "smtp.gmail.com",
-		:port                 => 587,
-		:user_name            => ENV['GMAIL_USERNAME'],
-		:password             => ENV['GMAIL_USERNAME'],
-		:authentication       => "plain",
+		:address        => "smtp.gmail.com",
+		:port           => 587,
+    :authentication => :login,
+    :user_name      => ENV['GMAIL_USERNAME'],
+    :password       => ENV['GMAIL_PASSWORD'],
+    :domain         => 'testcreator.com',
 		:enable_starttls_auto => true
 	}
   
