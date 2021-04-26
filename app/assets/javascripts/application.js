f62e1f71ce864f709dcde610a3b7cdad5c87f122
Andrei Loghin <soricel98@yahoo.com>
2021-04-26 23:12:34 +0300

    ilp fix attempt8

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require jquery3
 //= require popper
-//= require bootstrap
 //= require_tree .
