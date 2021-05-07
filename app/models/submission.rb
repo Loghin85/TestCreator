47cf1949b7d24e719af18a07a95d7592b1ac8f35
Andrei Loghin <soricel98@yahoo.com>
2021-05-07 19:24:18 +0300

    fixed submission with same id bug

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,7 +1,7 @@
 class Submission < ApplicationRecord
 	belongs_to :assessment
+	validates :userId, uniqueness: { case_sensitive: true, scope: :assessment_id }
 	validates :assessment_id, presence: true
-	validates :userId, uniqueness: { case_sensitive: true }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :userEmail, length: { maximum: 75 },
 												format: { with: VALID_EMAIL_REGEX }
